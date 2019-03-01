6920ffb0796dafac2659327140c98753724cb35d
Grant Smith <grant@labs.epiuse.com>
2019-03-01 11:06:58 +0200

    second commit

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,21 +1,21 @@
 <template>
-  <!-- TODO: 4 - Display individual stock details -->
+
   <div class="col-sm-6 col-md-4">
     <div class="panel panel-success">
       <div class="panel-heading">
         <h3 class="panel-title">
-          NAME
-          <small>(Price: PRICE)</small>
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
         </h3>
       </div>
       <div class="panel-body">
         <div class="pull-left">
-          <!-- TODO: 5 - Create a 'quantity' data property and bind it to the <input> -->
-          <input type="number" class="form-control" placeholder="Quantity">
+
+          <input v-model.number="quantity" type="number" class="form-control" placeholder="Quantity">
         </div>
         <div class="pull-right">
-          <!-- TODO: 7 - Disable the button if the quanity is negative or not an integer -->
-          <button class="btn btn-success">Buy</button>
+
+          <button class="btn btn-success" :disabled="quantity < 0 || !Number.isInteger(quantity)" @click="buyStock">Buy</button>
         </div>
       </div>
     </div>
@@ -29,12 +29,18 @@ export default {
             quantity: 0,
         };
     },
+    props: {
+      stock: {
+        type: Object,
+        required: true,
+      },
+    },
     methods: {
         buyStock() {
-            // TODO: 6 - Complete the 'order' object and bind this method to the button's click event
+
             const order = {
-                stockId: null,
-                stockPrice: null,
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
                 quantity: this.quantity,
             };
 
